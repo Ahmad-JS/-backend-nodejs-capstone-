e73457f9733c9d83531301f954954d2cd9500c7e
أحمد اللقماني الحربي <96401137+Ahmad-JS@users.noreply.github.com>
2024-03-27 07:47:54 +0300

    Update db.js

--- secondChance-backend/models/db.js
+++ secondChance-backend/models/db.js
@@ -1,3 +1,4 @@
+
 // db.js
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
@@ -6,23 +7,21 @@ const MongoClient = require('mongodb').MongoClient;
 let url = `${process.env.MONGO_URL}`;
 
 let dbInstance = null;
-const dbName = `${process.env.MONGO_DB}`;
+const dbName = "secondChance";
 
 async function connectToDatabase() {
     if (dbInstance){
         return dbInstance
     };
 
-    const client = new MongoClient(url);      
+    const client = new MongoClient(url);
 
     // Task 1: Connect to MongoDB
-    // {{insert code}}
-
+    await client.connect();
     // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
-
-    // Task 3: Return database instance
-    // {{insert code}}
+    dbInstance = client.db(dbName);
+// Task 3: Return database instance
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
